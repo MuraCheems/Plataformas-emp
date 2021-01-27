203b4539161cfa11a0ae6daa5e9a3b5b541243ca
MuraCHEEMS <juanj.duque@upb.edu.co>
2021-01-27 18:40:50 -0500

    Pagina de recordar contrasena finalizada con el script que le da al usuario su contrasena ingresando el email y el usuario

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-var form = document.getElementById("input-group");
-function handleForm(event) { event.preventDefault(); } 
-form.addEventListener('onclick', handleForm);
 
-var botongaleria = document.querySelector("#fruticas");
+
+
+
+
 function register(usuario,correo,contra,nombre)
 {
-
     if(localStorage.getItem(correo) == null&&correo!=""&&usuario!=""&&contra!=""&&nombre!="")
     {
         localStorage.setItem(nombre,correo);
         localStorage.setItem(correo,usuario);
         localStorage.setItem(usuario,contra)
         alert("Registro exitoso");
-
     }
     else{
         alert("Usuario ya existe")
     }
-
-    }
+  }
 
 
 function login(usuario,contra)
@@ -29,6 +26,17 @@ function login(usuario,contra)
       alert("Contresena equivocada o usuario incorrecto")
         }       
     }
+
+
+function recordar(usuario, correo){
+  if(localStorage.getItem(correo) == usuario){
+
+    alert("Su contrasena es :" + localStorage.getItem(usuario));
+  }else{
+
+    alert("El correo o el usuario no existen, revise lo que escribio");
+  }
+}
 
 
 
