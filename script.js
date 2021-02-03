659eac7e48a5410f16045be69048c49fc9a5a7b2
MuraCHEEMS <juanj.duque@upb.edu.co>
2021-02-03 16:22:20 -0500

    Se agrego una nueva imagen al about de mi, se cambio una imagen de la galeria de frutas, se agrego otra animacion al login/registro

--- script.js
+++ script.js
@@ -39,6 +39,24 @@ function recordar(usuario, correo){
 }
 
 
+var x = document.getElementById("login");
+var y = document.getElementById("registro");
+var z  = document.getElementById("btn");
+
+function registro(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";  
+}
+
+function loginanimation(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";  
+}
+
+
+
 
 
 
